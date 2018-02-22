309a415b0ae4cd5aa493c8f7a9925e8bddf5bd09
Ben Pyrik <ben@wsundine.com>
2018-02-21 20:22:05 -0500

    Update dist (0.1.3)
    Adds transparency

--- dist/aframe-cubemap-component.js
+++ dist/aframe-cubemap-component.js
@@ -90,6 +90,10 @@ AFRAME.registerComponent('cubemap', {
     ext: {
       type: 'string',
       default: 'jpg'
+    },
+    transparent: {
+      type: 'boolean',
+      default: false
     }
   },
 
@@ -125,7 +129,6 @@ AFRAME.registerComponent('cubemap', {
     loader.setPath(srcPath);
 
     var cubemap = loader.load(urls);
-    cubemap.format = THREE.RGBFormat;
 
     var shader = THREE.ShaderLib['cube']; // init cube shader from built-in lib
 
@@ -135,7 +138,8 @@ AFRAME.registerComponent('cubemap', {
       vertexShader: shader.vertexShader,
       uniforms: shader.uniforms,
       depthWrite: false,
-      side: THREE.BackSide
+      side: THREE.BackSide,
+      transparent: data.transparent
     });
 
     // Clone ShaderMaterial (necessary for multiple cubemaps)
